3c2b80c09f7cca740fbb6f7a099e10c9580521b2
Anirudh Pradhan <anipradhan.04@gmail.com>
2023-10-07 02:47:33 +0530

    add all changes

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def predict():
     features_std = standard_scaler.transform(features)
     prediction = model.predict(features_std)
     output = round(prediction[0], 2)
-    return render_template("index.html", prediction_text="The healthiness score is {}".format(output))
+    return render_template("index.html", prediction_text="The healthiness score is {}".format(output*2))
 
 if __name__ == "__main__":
     app.run(debug=True)
